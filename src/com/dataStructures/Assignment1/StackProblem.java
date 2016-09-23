c6b14ab1277e13b54c1befc1b52686cbb7173f4d
Harsh Rawat <harsh.rawat.ece11@iitbhu.ac.in>
2016-09-24 00:04:43 +0530

    Assignment 1 Part 1 Commit

--- src/com/dataStructures/Assignment1/StackProblem.java
+++ src/com/dataStructures/Assignment1/StackProblem.java
@@ -2,7 +2,57 @@ package src.com.dataStructures.Assignment1;
 
 import java.util.Scanner;
 
+import src.com.dataStructures.StackImpl.Stack;
+
 public class StackProblem {
 	
+	Stack<Character> stack;
+	
+	public StackProblem() {
+		this.stack = new Stack<>();
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Enter the String to be Checked");
+		Scanner sc =  new Scanner(System.in);
+		String data = sc.nextLine();
+		
+		StackProblem obj = new StackProblem();
+		obj.performCheck(data);
+	}
+	
+	/**
+	 * This function performs the check and prints Success if entire data is in correct format.
+	 * Or it prints position where the discrepency occurs.
+	 * */
+	public void performCheck(String data){
+		Character ch,output;
+		int length = data.length();
+		int count = 0;
+		
+		while(count < length)
+		{
+			ch = data.charAt(count);
+			if(ch == '(' || ch == '{' || ch == '[')
+			{
+				stack.push(ch);
+			}
+			else if(ch == ')' || ch == '}' || ch == ']'){
+				output = stack.pop();
+				if((ch ==')' && output !='(') || (ch =='}' && output !='{') || (ch ==']' && output !='[')){
+					System.out.println(count+1);
+					System.exit(-1);
+				}
+			}
+			count++;
+		}
+		
+		if(stack.isEmpty())
+			System.out.println("Success");
+		else
+			System.out.println(count);
+		
+	}
 
 }
